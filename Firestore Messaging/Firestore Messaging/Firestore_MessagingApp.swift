584b3155c85e95111e6e41732ce12827b9090ce8
Michael Parekh <mparekh4@illinois.edu>
2023-04-12 17:33:37 -0400

    add app icon and color set

---
+++ Firestore Messaging/Firestore Messaging/Firestore_MessagingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firestore_MessagingApp.swift
+//  Firestore Messaging
+//
+//  Created by Michael Parekh on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Firestore_MessagingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
