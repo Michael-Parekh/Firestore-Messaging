8cf50d1a717c8735bdb3642bc831cbb80d0f1735
Michael Parekh <mparekh4@illinois.edu>
2023-04-13 11:20:31 -0400

    init firestore using 'FirebaseFirestore’ and 'FirebaseFirestoreSwift'

--- Firestore Messaging/Firestore Messaging/Firestore_MessagingApp.swift
+++ Firestore Messaging/Firestore Messaging/Firestore_MessagingApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct Firestore_MessagingApp: App {
+    // Installed the ‘FirebaseFirestore’ and ‘FirebaseFirestoreSwift’ packages (‘FirebaseFirestoreSwift’ allows us to convert the Firestore data into our ‘Message’ model).
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
